58a7b4b0afd57c3dc38c8de42590d36dabb7f02c
Mateus Neri <mateuslimaneri@gmail.com>
2022-02-06 18:28:19 -0300

    #02 - adição de assets; criação das telas de login, cadastro e splash; lógica das rotas; conexão com o backend

---
+++ lib/app/modules/auth/register/register_page.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:vakinha_burger_mobile/app/core/ui/vakinha_ui.dart';
+import 'package:vakinha_burger_mobile/app/core/ui/widgets/vakinha_appbar.dart';
+import 'package:vakinha_burger_mobile/app/core/ui/widgets/vakinha_button.dart';
+import 'package:vakinha_burger_mobile/app/core/ui/widgets/vakinha_textformfield.dart';
+
+class RegisterPage extends StatelessWidget {
+  const RegisterPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: VakinhaAppBar(
+        elevation: 0,
+      ),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: IntrinsicHeight(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Form(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Cadastro',
+                    style: context.textTheme.headline6?.copyWith(
+                      fontWeight: FontWeight.bold,
+                      color: context.theme.primaryColorDark,
+                    ),
+                  ),
+                  Text(
+                    'Preencha os campos abaixo para criar o seu cadastro',
+                    style: context.textTheme.bodyText1,
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  const VakinhaTextFormField(
+                    label: 'Nome',
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  const VakinhaTextFormField(
+                    label: 'E-mail',
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  const VakinhaTextFormField(
+                    label: 'Senha',
+                    obscureText: true,
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  const VakinhaTextFormField(
+                    label: 'Confirmar Senha',
+                    obscureText: true,
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Center(
+                    child: VakinhaButton(
+                      label: 'Entrar',
+                      width: context.width,
+                      onPressed: () {},
+                    ),
+                  ),
+                ]
+              )
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
